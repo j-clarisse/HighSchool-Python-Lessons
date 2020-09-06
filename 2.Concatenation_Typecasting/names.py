082ba6138ec6cc4259f2e8472dc41367758ec770
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2020-09-06 14:23:48 +1000

    Week 2

---
+++ 2.Concatenation_Typecasting/names.py
@@ -0,0 +1,12 @@
+# Name: Names Program
+# Date:
+# Author:
+# Description: Program asks the user for their first name and then asks for their last name. The program must then print these together.
+
+# HINT: Use the input() function (don’t forget to store it in a variable) and remember to concatenate (‘,’ or ‘+’)!! 
+
+
+# Answer:
+first = input("Enter firstname: ")
+last = input("Enter lastname: ")
+print("Your name is", first, last)
---
+++ 2.Concatenation_Typecasting/typecasting.py
@@ -0,0 +1,14 @@
+# Name: Simple Addition Calculator
+# Date:
+# Author:
+# Description: Program asks the user for two numbers one at a time, and prints out the sum of those two numbers.
+
+""" Typecasting changes the type of a variable.
+str(variable)
+int(variable)
+float(variable) """
+
+# Answer: 
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print(num1,"+", num2, "=", str(int(num1)+int(num2)))
