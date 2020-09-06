f064012d575a8ed8af41a877fa84fb703c92979d
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2020-09-06 14:54:07 +1000

    Week 2

---
+++ 2.Concatenation_Typecasting/bonusmarks.py
@@ -0,0 +1,9 @@
+# Name: Bonus Marks Program
+# Date:
+# Author:
+# Description: Write a program that takes an integer input (a student’s mark) and adds an extra 10 marks. The program should output the old mark and the new mark.
+
+# Answer:
+oldMark = input("Enter student's mark: ")
+newMark = str(int(oldMark)+10)
+print("Old mark:",oldMark+"\n"+"New mark:", newMark)
