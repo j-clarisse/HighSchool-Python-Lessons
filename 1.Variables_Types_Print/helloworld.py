95fe111c0b9aa4fa6e809162b8a7f38c329fd4f0
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2020-09-06 14:16:21 +1000

    Week 1

---
+++ 1.Variables_Types_Print/helloworld.py
@@ -0,0 +1,9 @@
+# Name: Hello World Program
+# Date:
+# Author:
+# Description: Program prints 'Hello World!'
+
+
+
+# Answer:
+print("Hello World!")
---
+++ 1.Variables_Types_Print/types.py
@@ -0,0 +1,18 @@
+# Types Program
+# Date:
+# Author:
+# Description: Program experiments with variable types: integer, string, float
+
+
+
+# Answer:
+string = "this is a string"
+integer = 1
+flt = 2.5
+
+print(string)
+print(type(string))
+print(integer)
+print(type(integer))
+print(flt)
+print(type(flt))
